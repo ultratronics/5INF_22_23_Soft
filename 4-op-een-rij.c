c2168b4f2785a1a1ad8f061ce8401ef0a0f38a2b
ultratronics <46576909+ultratronics@users.noreply.github.com>
2023-01-24 09:19:21 +0100

    Add files via upload

--- 4-op-een-rij.c
+++ 4-op-een-rij.c
@@ -4,6 +4,19 @@
 int wieIsAanDeBeurt = 0; //2 spelers
 char bord[6][7]; //bord
 void printt();
+
+void initialiseerBord(){
+
+for (int i = 0; i < 6; i++) {
+ for (int j = 0; j < 7; j++) {
+   bord[i][j] = 'L';
+   //printf("%c\n",bord[i][j] );
+ }
+}
+
+}
+
+
 int jeton(int kolom){
 
   for (int i = 5; i >= 0; i--) {
@@ -29,7 +42,7 @@ int jeton(int kolom){
 }
 
 void printt(){
-
+printf("** 1 ** 2 ** 3 ** 4 ** 5 ** 6 ** 7\n");
 printf("****************\n");
   for (int i = 0; i < 6; i++) {
    for (int j = 0; j < 7; j++) {
@@ -49,27 +62,16 @@ int main() {
   /* code */
 
 //X O L
+initialiseerBord(); // maak bord klaar
 
-
-
-for (int i = 0; i < 6; i++) {
- for (int j = 0; j < 7; j++) {
-   bord[i][j] = 'L';
-   printf("%c\n",bord[i][j] );
- }
-}
-
-jeton(2);
+while(1){
+// Vragen voor input
+printf("Waar wil je een jeton ingooien?\n");
+int waarde;
+scanf("%i",&waarde);
+jeton(waarde-1); //kolom 3 , tellen vanaf 0
 printt();
-
-for (int i = 0; i < 6; i++) {
- printf("***************************\n");
- //printf("\t \t \t \t \t ")
-
-
-
 }
-
 
   return 0;
 }
